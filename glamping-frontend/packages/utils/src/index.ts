7a091f9c56ecd4664aef28caed27a07becf2730c
gleb <smirnovg2005@gmail.com>
2026-07-13 22:35:43 +0300

    chore: remove unused mock data files

--- glamping-frontend/packages/utils/src/index.ts
+++ glamping-frontend/packages/utils/src/index.ts
@@ -1,9 +1,3 @@
 export const formatDate = (date: Date): string => date.toISOString()
 
-export * from './mocks/tasks'
-export * from './mocks/menuItems'
-export * from './mocks/services'
-export * from './mocks/houses'
-export * from './mocks/messages'
-
 export { default as i18n } from './i18n'
